2e27d502a9aa9c81da9d0c2ab348f4921ced8577
harunou <dev.harunou@gmail.com>
2026-09-09 09:38:14 +0200

    Orders: export only the units the app places

--- lib/features/orders/orders.dart
+++ lib/features/orders/orders.dart
@@ -1,6 +1,2 @@
 export 'drivers/toast_driver.dart';
-export 'widgets/order/order.dart';
-export 'widgets/order_item/order_item.dart';
 export 'widgets/orders.dart';
-export 'widgets/orders_resource_picker.dart';
-export 'widgets/orders_statistics.dart';
